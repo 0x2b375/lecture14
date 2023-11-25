eb65f27ef6842db181042abc8d3a9924368a97bd
0xb39 <giiguu.giigu@gmail.com>
2023-11-25 16:36:11 +0800

    update StudentTableRow

--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 export default class StudentTableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteStudent = this.deleteStudent.bind(this);
+  }
+  deleteStudent() {
+    axios
+      .delete(
+        "http://localhost:4000/students/delete-student/" + this.props.obj._id
+      )
+      .then((res) => {
+        console.log("Student successfully deleted!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <tr>
